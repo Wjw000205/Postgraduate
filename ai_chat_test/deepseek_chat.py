7ff14c1e38385e7f758081d35d63848ab2c5c775
33932 <3393225681@qq.com>
2025-04-25 19:20:29 +0800

    2025.4.25 聊天ai，拆分文件

--- ai_chat_test/deepseek_chat.py
+++ ai_chat_test/deepseek_chat.py
@@ -1,54 +1,6 @@
 import json
-from add import add
-from openai import OpenAI
-from linear_regression import linear_regression
+from ai_chat_test.util import send_messages, use_tools
 from tools import tools
-
-client = OpenAI(
-    api_key="sk-d02e930a39e2441a9f345cfdaddae735",
-    base_url="https://api.deepseek.com",
-)
-
-def send_messages(messages,tools = None):
-    response = client.chat.completions.create(
-        model="deepseek-chat",
-        messages=messages,
-        tools=tools,
-        # response_format={
-        #     'type': 'json_object'
-        # }
-    )
-    return response
-
-def use_tools(json_data):
-    function_name = json.loads(json_data)["choices"][0]["message"]["tool_calls"][0]["function"]["name"]
-    arguments = json.loads(json_data)["choices"][0]["message"]["tool_calls"][0]["function"]["arguments"]
-
-    if function_name == "linear_regression":
-        function_mapper = {
-            "linear_regression": linear_regression
-        }
-
-    elif function_name == "add":
-        function_mapper = {
-            "add": add
-        }
-    function = function_mapper.get(function_name)
-    if arguments == {}:
-        function_output = "No arguments provided."
-        print("No arguments")
-    else:
-        function_output = function(json.loads(arguments)["x"], json.loads(arguments)["y"])
-        print(f"AI:{function_output}\n")
-        messages = [
-            {"role": "user",
-                "content": "Please summarize the process of my tool usage." + str(function_output)},
-            {"role": "system", "content": ""}
-        ]
-        answer = send_messages(messages, tools)
-        print(json.dumps(answer.to_dict()))
-        answer_data = json.loads(json.dumps(answer.to_dict()))["choices"][0]["message"]["content"]
-        print(f"AI:{answer_data}\n")
 
 def main_loop():
     print("Welcome to DeepSeek")
@@ -58,6 +10,7 @@ def main_loop():
             user_input = input("user input:(input 'exit' to quit):")
 
             if user_input == "exit":
+                print("Goodbye!")
                 break
 
             messages = [
---
+++ ai_chat_test/util.py
@@ -0,0 +1,53 @@
+import json
+
+from openai import OpenAI
+
+from ai_chat_test.add import add
+from ai_chat_test.linear_regression import linear_regression
+from ai_chat_test.tools import tools
+
+client = OpenAI(
+    api_key="my_api_key",
+    base_url="https://api.deepseek.com",
+)
+
+def send_messages(messages,tools = None):
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=messages,
+        tools=tools,
+        # response_format={
+        #     'type': 'json_object'
+        # }
+    )
+    return response
+
+def use_tools(json_data):
+    function_name = json.loads(json_data)["choices"][0]["message"]["tool_calls"][0]["function"]["name"]
+    arguments = json.loads(json_data)["choices"][0]["message"]["tool_calls"][0]["function"]["arguments"]
+
+    if function_name == "linear_regression":
+        function_mapper = {
+            "linear_regression": linear_regression
+        }
+
+    elif function_name == "add":
+        function_mapper = {
+            "add": add
+        }
+    function = function_mapper.get(function_name)
+    if arguments == {}:
+        function_output = "No arguments provided."
+        print("No arguments")
+    else:
+        function_output = function(json.loads(arguments)["x"], json.loads(arguments)["y"])
+        print(f"AI:{function_output}\n")
+        messages = [
+            {"role": "user",
+                "content": "Please summarize the process of my tool usage." + str(function_output)},
+            {"role": "system", "content": ""}
+        ]
+        answer = send_messages(messages, tools)
+        print(json.dumps(answer.to_dict()))
+        answer_data = json.loads(json.dumps(answer.to_dict()))["choices"][0]["message"]["content"]
+        print(f"AI:{answer_data}\n")
